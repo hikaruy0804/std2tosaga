76d8266a7844eaa4b22f5f7d65f5914ae14dd211
hikaruy0804 <88568970+hikaruy0804@users.noreply.github.com>
2023-06-05 10:06:01 +0900

    Update fix_std2saga.py

--- fix_std2saga.py
+++ fix_std2saga.py
@@ -5,7 +5,7 @@ import unicodedata
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 class Std2saga:
-    def __init__(self, model_name_or_path="sonoisa/t5-base-japanese", model_dir="model", max_input_length=64, max_target_length=64,): #train_batch_size=64, eval_batch_size=64, num_train_epochs=20):
+    def __init__(self, model_name_or_path="sonoisa/t5-base-japanese", model_dir="model", max_input_length=64, max_target_length=64, csv_path='方言辞書.csv'): #train_batch_size=64, eval_batch_size=64, num_train_epochs=20):
         self.model_name_or_path = model_name_or_path
         self.model_dir = model_dir
         self.max_input_length = max_input_length
@@ -16,24 +16,26 @@ class Std2saga:
         
         self.tokenizer = T5Tokenizer.from_pretrained(self.model_dir, is_fast=True)
         self.trained_model = T5ForConditionalGeneration.from_pretrained(self.model_dir)
+        self.trained_model.eval()  # Set the model in evaluation mode
+        self.dictionary = self.create_dictionary_from_csv(csv_path)  # Load dictionary at the initialization
 
-    def unicode_normalize(self, cls, s):
-        pt = re.compile('([{}]+)'.format(cls))
-        def norm(c):
-            return unicodedata.normalize('NFKC', c) if pt.match(c) else c
-        s = ''.join(norm(x) for x in re.split(pt, s))
-        s = re.sub('－', '-', s)
-        return s
+#     def unicode_normalize(self, cls, s): #軽量化のためコメントアウト
+#         pt = re.compile('([{}]+)'.format(cls))
+#         def norm(c):
+#             return unicodedata.normalize('NFKC', c) if pt.match(c) else c
+#         s = ''.join(norm(x) for x in re.split(pt, s))
+#         s = re.sub('－', '-', s)
+#         return s
 
-    def remove_extra_spaces(self, s):
-        s = re.sub('[ 　]+', ' ', s)
-        blocks = ''.join(('\u4E00-\u9FFF',  # CJK UNIFIED IDEOGRAPHS
-                        '\u3040-\u309F',  # HIRAGANA
-                        '\u30A0-\u30FF',  # KATAKANA
-                        '\u3000-\u303F',  # CJK SYMBOLS AND PUNCTUATION
-                        '\uFF00-\uFFEF'   # HALFWIDTH AND FULLWIDTH FORMS
-                        ))
-        basic_latin = '\u0000-\u007F'
+#     def remove_extra_spaces(self, s): #軽量化のためコメントアウト
+#         s = re.sub('[ 　]+', ' ', s)
+#         blocks = ''.join(('\u4E00-\u9FFF',  # CJK UNIFIED IDEOGRAPHS
+#                         '\u3040-\u309F',  # HIRAGANA
+#                         '\u30A0-\u30FF',  # KATAKANA
+#                         '\u3000-\u303F',  # CJK SYMBOLS AND PUNCTUATION
+#                         '\uFF00-\uFFEF'   # HALFWIDTH AND FULLWIDTH FORMS
+#                         ))
+#         basic_latin = '\u0000-\u007F'
 
         def remove_space_between(cls1, cls2, s):
             p = re.compile('([{}]) ([{}])'.format(cls1, cls2))
@@ -46,24 +48,24 @@ class Std2saga:
         s = remove_space_between(basic_latin, blocks, s)
         return s
 
-    def normalize_neologd(self, s):
-        s = s.strip()
-        s = self.unicode_normalize('０-９Ａ-Ｚａ-ｚ｡-ﾟ', s)
+#     def normalize_neologd(self, s):#軽量化のためコメントアウト
+#         s = s.strip()
+#         s = self.unicode_normalize('０-９Ａ-Ｚａ-ｚ｡-ﾟ', s)
 
-        def maketrans(f, t):
-            return {ord(x): ord(y) for x, y in zip(f, t)}
+#         def maketrans(f, t):
+#             return {ord(x): ord(y) for x, y in zip(f, t)}
 
-        s = re.sub('[﹣－ｰ—―─━ー]+', 'ー', s)  # normalize choonpus
-        s = re.sub('[~∼∾〜〰～]+', '〜', s)  # normalize tildes (modified by Isao Sonobe)
-        s = s.translate(
-            maketrans('!"#$%&\'()*+,-./:;<=>?@[¥]^_`{|}~｡､･｢｣',
-                '！”＃＄％＆’（）＊＋，－．／：；＜＝＞？＠［￥］＾＿｀｛｜｝〜。、・「」'))
+#         s = re.sub('[﹣－ｰ—―─━ー]+', 'ー', s)  # normalize choonpus
+#         s = re.sub('[~∼∾〜〰～]+', '〜', s)  # normalize tildes (modified by Isao Sonobe)
+#         s = s.translate(
+#             maketrans('!"#$%&\'()*+,-./:;<=>?@[¥]^_`{|}~｡､･｢｣',
+#                 '！”＃＄％＆’（）＊＋，－．／：；＜＝＞？＠［￥］＾＿｀｛｜｝〜。、・「」'))
 
-        s = self.remove_extra_spaces(s)
-        s = self.unicode_normalize('！”＃＄％＆’（）＊＋，－．／：；＜＞？＠［￥］＾＿｀｛｜｝〜', s)  # keep ＝,・,「,」
-        s = re.sub('[’]', '\'', s)
-        s = re.sub('[”]', '"', s)
-        return s
+#         s = self.remove_extra_spaces(s)
+#         s = self.unicode_normalize('！”＃＄％＆’（）＊＋，－．／：；＜＞？＠［￥］＾＿｀｛｜｝〜', s)  # keep ＝,・,「,」
+#         s = re.sub('[’]', '\'', s)
+#         s = re.sub('[”]', '"', s)
+#         return s
 
     def remove_brackets(self, text):
         text = re.sub(r"(^【[^】]*】)|(【[^】]*】$)", "", text)
@@ -73,7 +75,7 @@ class Std2saga:
         assert "\n" not in text and "\r" not in text
         text = text.replace("\t", " ")
         text = text.strip()
-        text = self.normalize_neologd(text)
+        #text = self.normalize_neologd(text) #軽量化のためコメントアウト
         text = text.lower()
         return text
 
@@ -90,18 +92,12 @@ class Std2saga:
         return dictionary
 
     def sagaben(self, text):
-        # CSVファイルのパスを指定して辞書を読み込む
-        csv_path = '方言辞書.csv'
-        dictionary = self.create_dictionary_from_csv(csv_path)
-
         # 辞書でtextと一致する場合は、対応するsagaを出力
-        if text in dictionary:
+        if text in self.dictionary:
             time.sleep(0.3)
-            return dictionary[text]
+            return self.dictionary[text]
 
         else:
-            self.trained_model.eval()
-
             inputs = [self.preprocess_body(text)]
             batch = self.tokenizer.batch_encode_plus(
                 inputs, max_length=self.max_input_length, truncation=True, 
